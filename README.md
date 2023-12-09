1fddf025711056dddf6146642bbc99c2bdf07040
YusnelG <yusnelleyvag@gmail.com>
2023-12-09 10:58:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# MiniGrep
+
+MiniGrep is a command-line utility written in Rust for searching text within files.
+
+## Usage
+
+To use MiniGrep, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/your-username/MiniGrep.git`
+2. Navigate to the project directory: `cd MiniGrep`
+3. Build the project: `cargo build`
+4. Run MiniGrep: `cargo run <text> <filename>`
+
+Replace `<text>` with the text you want to search for, and `<filename>` with the name of the file you want to search in.
+
+## Example
+
+Run MiniGrep: `cargo run model test.txt`
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Usage: minigrep <text> <filename>");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in contents.lines() {
+        if line.contains(&config.query) {
+            println!("{}", line);
+        }
+    }
+
+    if contents.lines().any(|line| line.contains(&config.query)) {
+        Ok(())
+    } else {
+        Err("No matches found")?
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: minigrep <text> <filename>");
+        std::process::exit(1);
+    }
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Error parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
